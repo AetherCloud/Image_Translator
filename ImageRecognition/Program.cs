d7aec15dee43f6bd0c3e5d53e446b108c7f298fe
frbl <frbl@itu.dk>
2018-06-26 18:45:34 +0200

    Cannot find enviroment variable

---
+++ ImageRecognition/Program.cs
@@ -0,0 +1,44 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Vision.V1;
+using System;
+
+namespace ImageRecognition
+{
+    class Program
+    {
+        static void runner(string filePath)
+        {
+            // Load an image from a local file.
+            var image = Image.FromFile(filePath);
+            var client = ImageAnnotatorClient.Create();
+            var response = client.DetectText(image);
+            foreach (var annotation in response)
+            {
+                if (annotation.Description != null)
+                    Console.WriteLine(annotation.Description);
+            }
+        }
+
+   
+
+
+        static void Main(string[] args)
+        {
+            var credential = GoogleCredential.FromFile("C:\\Users\\frede\\Desktop\\Private projects\\ImageRecognition\\My First Project-873f16711d42.json");
+            // Instantiates a client
+            
+            //var client = ImageAnnotatorClient.Create(credential);
+            // Load the image file into memory
+            var image = Image.FromFile("wakeupcat.jpg");
+            // Performs label detection on the image file
+            var response = client.DetectLabels(image);
+            foreach (var annotation in response)
+            {
+                if (annotation.Description != null)
+                    Console.WriteLine(annotation.Description);
+            }
+            string value = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS");
+            runner("C:\\Users\\frede\\source\\repos\\ImageRecognition\\Resources\\TextTests.png");
+        }
+    }
+}
